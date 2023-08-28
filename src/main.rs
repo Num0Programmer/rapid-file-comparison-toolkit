332be4253683743aff7eae2545375b6a267a8563
Num0Programmer <asajh4@gmail.com>
2023-08-27 17:20:51 -0700

    Added checking for when a provided file does not exist. Added a check for when file lengths are not the same. Imported anyhow and log packages.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,36 @@
-fn main()
+use std::env;
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+
+use log::{info, warn, error};
+
+// arg selecting
+const FILE_ONE_SEL: usize = 1;
+const FILE_TWO_SEL: usize = 2;
+
+
+fn main() -> std::io::Result<()>
 {
-    let total_lines = 0; // total lines in file one
-    let mut lines_equal = 0;
-    let mut processed_lines = 0;
-    
+    let args: Vec<_> = env::args().collect();
+    let file_1_str = &args[FILE_ONE_SEL];
+    let file_2_str = &args[FILE_TWO_SEL];
+
+    // TODO: impl custom errors
     // try to open first file
+    let file_1 = File::open(&file_1_str)?;
 
     // try to open second file
+    let file_2 = File::open(&file_2_str)?;
+
+    let total_lines = fs::metadata(&file_1_str)?.len(); // total lines in file one
+    let mut lines_equal = 0;
+    let mut processed_lines = 0;    
 
     // check files have same length
+    if total_lines != fs::metadata(&file_2_str)?.len()
     {
-        // log warning for different length files
+        warn!("Warning: The length of provided files is not the same!");
     }
 
     // create BufReaders for files
@@ -32,5 +52,7 @@ fn main()
 
     // log number of lines processed
     // log number of lines equal out of total lines in first file
+
+    Ok(())
 }
 
