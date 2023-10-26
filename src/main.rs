e69c3cf9eee5b030485dfbc038c7be2e8e350b2d
Num0Programmer <asajh4@gmail.com>
2023-10-25 23:17:33 -0700

    Added a way for the user to view general usage information (i.e. the different filesystem objects they can pass in.

--- src/main.rs
+++ src/main.rs
@@ -6,42 +6,62 @@ use rfctk::*;
 fn main() -> std::io::Result<()>
 {
     // establish environment
-    let env_args: Vec<_> = env::args().collect();
+    let env_args: Vec<String> = env::args().collect();
 
-    // initialize statistics structure
-    let mut stats = ComparisonStats::new();
-
-    let key_tup = (
-        fs::metadata(&env_args[ARG_ONE_SEL])?.is_dir(),
-        fs::metadata(&env_args[ARG_TWO_SEL])?.is_dir()
-    );
-    match key_tup
+    // check user wants help
+    if env_args[ARG_ONE_SEL].eq("help")
     {
-        // check for directory to directory comparison
-        (true, true) =>
+        help();
+    }
+    // otherwise, assume user wants to compare filesystem objects
+    else
+    {
+        // initialize statistics structure
+        let mut stats = ComparisonStats::new();
+        // collect filesystem object names for comparison
+        let key_tup = (
+            fs::metadata(&env_args[ARG_ONE_SEL])?.is_dir(),
+            fs::metadata(&env_args[ARG_TWO_SEL])?.is_dir()
+        );
+
+        match key_tup
         {
-            println!("Directory to directory comparison is not supported yet!");
-        },
-        // check for directory to file comparison
-        (true, false) =>
-        {
-            dir_file_cmp(&mut stats, &env_args[ARG_ONE_SEL], &env_args[ARG_TWO_SEL])?;
-        },
-        (false, true) =>
-        {
-            dir_file_cmp(&mut stats, &env_args[ARG_TWO_SEL], &env_args[ARG_ONE_SEL])?;
-        },
-        // assume file to file comparison
-        _ =>
-        {
-            file_cmp(&mut stats, &env_args[ARG_ONE_SEL], &env_args[ARG_TWO_SEL])?;
+            // check for directory to directory comparison
+            (true, true) =>
+            {
+                println!("Directory to directory comparison is not supported yet!");
+            },
+            // check for directory to file comparison
+            (true, false) =>
+            {
+                dir_file_cmp(&mut stats, &env_args[ARG_ONE_SEL], &env_args[ARG_TWO_SEL])?;
+            },
+            (false, true) =>
+            {
+                dir_file_cmp(&mut stats, &env_args[ARG_TWO_SEL], &env_args[ARG_ONE_SEL])?;
+            },
+            // assume file to file comparison
+            _ =>
+            {
+                file_cmp(&mut stats, &env_args[ARG_ONE_SEL], &env_args[ARG_TWO_SEL])?;
+            }
         }
+
+        // give report of comparison
+        println!("{} lines processed", stats.total_lines_processed);
+        println!("{} out of {} lines were equivalent.",
+            stats.total_lines_equal, stats.total_lines_processed
+        );
     }
 
-    println!("{} lines processed", stats.total_lines_processed);
-    println!("{} out of {} lines were equivalent.",
-        stats.total_lines_equal, stats.total_lines_processed
-    );
-
     Ok(())
+}
+
+
+fn help()
+{
+    // present general usage information
+    println!("usage: rfc <file_1> <file_2>");
+    println!("           <file_1> <dir>");
+    println!("           <dir_1>  <dir_2>");
 }
